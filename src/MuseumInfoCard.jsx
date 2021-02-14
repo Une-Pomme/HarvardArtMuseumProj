f83fb976ccfab470565e89a4a12a583cf3207f5e
Lianne McIntosh <5875300+Une-Pomme@users.noreply.github.com>
2021-02-14 00:49:18 -0500

    Resizes the image in the MuseumInfoCard component so it doesn't take up the entire screen. Lets user know if there isn't an image attached to the object

--- src/MuseumInfoCard.jsx
+++ src/MuseumInfoCard.jsx
@@ -3,6 +3,7 @@ import {
     Card, CardText, CardBody,
     CardTitle, CardSubtitle, CardLink, CardImg 
   } from 'reactstrap';
+  import './MuseumInfoCard.css';
 
 const MuseumInfoCard = (props) =>{
     const {info} = props;
@@ -12,8 +13,8 @@ const MuseumInfoCard = (props) =>{
     <CardTitle tag="h5">{info.title}</CardTitle>
     <CardSubtitle tag="h6" className="mb-2 text-muted">Date: {info.dated}</CardSubtitle>
     {info.imagecount > 0 && info.images.length > 0? 
-    <CardImg src={info.images[0].baseimageurl} alt={info.images[0].alttext !== null ? info.images[0].alttext : "" }></CardImg>   :
-    null 
+    <CardImg className="museum-object-image" src={info.images[0].baseimageurl} alt={info.images[0].alttext !== null ? info.images[0].alttext : "" }></CardImg>   :
+    <p> [No image is available]</p>
 }
     {info.description !== null? 
     <CardText>Object Description: {info.description}</CardText> :
